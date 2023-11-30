9614c13312143d277585f5cee806ea884cf1d72a
mtsonkova <mtsonkova950@gmail.com>
2023-11-30 22:48:18 +0200

    Added new folder ExerciseObjectsAndClasses in Programming Advanced for QA. Problem 1 of 4 solved.

---
+++ Programming Advanced for QA/ExerciseObjectsAndClasses/Students/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Students
+{
+    public class Student
+    {
+        public string firstName { get; set; }
+       public string lastName { get; set; }
+        public double grade { get; set; }
+
+        public Student(string firstName, string lastName, double grade)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.grade = grade;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            int studentsCount = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < studentsCount; i++)
+            {
+                string[] studentInfo = Console.ReadLine().Split(" ").ToArray();
+                string firstName = studentInfo[0];
+                string lastName = studentInfo[1];
+                double grade = double.Parse(studentInfo[2]);
+
+                Student student = new Student(firstName, lastName, grade);
+                
+                students.Add(student);
+                
+            }
+
+           var sortedList = students.OrderByDescending(student => student.grade);
+            foreach(Student student in sortedList)
+            {
+                Console.WriteLine($"{student.firstName} {student.lastName} {student.grade}");
+            }
+
+        }
+    }
+}
