305efcc263f2725406a186bc3d5c5f14579ae5da
mtsonkova <mtsonkova950@gmail.com>
2023-12-04 22:22:32 +0200

    task in progress.

---
+++ Programming Advanced for QA/15-Resources-Implementing-OOP-Hierarchy/Order.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseOopHierarchy
+{
+    internal class Order
+    {
+        private List<MenuItem> _items = new List<MenuItem>();
+
+        public void addItem(MenuItem item)
+        {
+           this._items.Add(item);
+        }
+
+        public decimal getTotal(List<MenuItem> items)
+        {
+            decimal total = 0;
+            foreach(MenuItem item in items)
+            {
+                total += item.Price;    
+            }
+            return total;
+        }
+
+        public IReadOnlyCollection<MenuItem> Items => this._items.AsReadOnly();
+            }
+
+
+    }
+
