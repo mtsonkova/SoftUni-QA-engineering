fbb334ff84ba391e2a72e12769b01d975f9eff3e
mtsonkova <mtsonkova950@gmail.com>
2023-12-06 21:07:06 +0200

    Work in progress.

---
+++ Programming Advanced for QA/15-Resources-Implementing-OOP-Hierarchy/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseOopHierarchy
+{
+    internal class Customer
+    {
+        private List<MenuItem> _orderHistory;
+        public Name
+        •	Field _orderHistory – list of menu items.
+•	Property Name – string.
+•	Property Email – string.
+•	Property OrderHistory – read only order collection from _orderHistrory.
+•	Method override ToString() - "{this.Name} - {this.Description} - ${this.Price}".
+•	Constructor accepting the two properties.
+•	Method AddOrder(Order order) – adds the given order to the _orderHistory list.
+
+    }
+}
