d68427b4d72bcc4bac9cca97e63acb93c0c8323d
mtsonkova <mtsonkova950@gmail.com>
2024-01-02 00:42:19 +0200

    Fruits UnitTests finished.

--- Programming Advanced for QA/ExamPreparation2/02-Fruits-Resources/TestApp.Tests/FruitsTests.cs
+++ Programming Advanced for QA/ExamPreparation2/02-Fruits-Resources/TestApp.Tests/FruitsTests.cs
@@ -10,30 +10,105 @@ public class FruitsTests
     [Test]
     public void Test_GetFruitQuantity_FruitExists_ReturnsQuantity()
     {
-        // TODO: finish this test
+        //Arrange
+        Dictionary<string, int> fruitDictionary = new Dictionary<string, int>()
+        {
+            { "orange", 3 },
+            { "lemon", 12 },
+            { "banana", 5 },
+            { "cherry", 4 },
+            { "pear", 11 },
+            { "apple", 6 }
+
+        };
+
+        string fruit = "cherry";
+
+        // Act
+        int qty = Fruits.GetFruitQuantity(fruitDictionary, fruit);
+
+        // Assert
+        Assert.That(qty, Is.EqualTo(4));
     }
 
     [Test]
     public void Test_GetFruitQuantity_FruitDoesNotExist_ReturnsZero()
     {
-        // TODO: finish this test
+        //Arrange
+        Dictionary<string, int> fruitDictionary = new Dictionary<string, int>()
+        {
+            { "orange", 3 },
+            { "lemon", 12 },
+            { "banana", 5 },
+            { "cherry", 4 },
+            { "pear", 11 },
+            { "apple", 6 }
+
+        };
+
+        string fruit = "strawberry";
+
+        // Act
+        int qty = Fruits.GetFruitQuantity(fruitDictionary, fruit);
+
+        // Assert
+        Assert.That(qty, Is.EqualTo(0));
     }
 
     [Test]
     public void Test_GetFruitQuantity_EmptyDictionary_ReturnsZero()
     {
-        // TODO: finish this test
+        //Arrange
+        Dictionary<string, int> fruitDictionary = new Dictionary<string, int>();
+        
+
+        string fruit = "cherry";
+
+        // Act
+        int qty = Fruits.GetFruitQuantity(fruitDictionary, fruit);
+
+        // Assert
+        Assert.That(qty, Is.EqualTo(0));
     }
 
     [Test]
     public void Test_GetFruitQuantity_NullDictionary_ReturnsZero()
     {
-        // TODO: finish this test
+        // Arrange
+        Dictionary<string, int> fruitDictionary = null;
+
+
+        string fruit = "cherry";
+
+        // Act
+        int qty = Fruits.GetFruitQuantity(fruitDictionary, fruit);
+
+        // Assert
+        Assert.That(qty, Is.EqualTo(0));
     }
 
     [Test]
     public void Test_GetFruitQuantity_NullFruitName_ReturnsZero()
     {
-        // TODO: finish this test
+        // Arrange
+        Dictionary<string, int> fruitDictionary = new Dictionary<string, int>()
+        {
+            { "orange", 3 },
+            { "lemon", 12 },
+            { "banana", 5 },
+            { "cherry", 4 },
+            { "pear", 11 },
+            { "apple", 6 }
+
+        };
+
+
+        string fruit = null;
+
+        // Act
+        int qty = Fruits.GetFruitQuantity(fruitDictionary, fruit);
+
+        // Assert
+        Assert.That(qty, Is.EqualTo(0));
     }
 }
