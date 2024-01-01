c2fc33b12527b4130a258edf2ac2119de69a43ac
mtsonkova <mtsonkova950@gmail.com>
2024-01-02 01:01:06 +0200

    CSV-Parser unit tests completed.

--- Programming Advanced for QA/ExamPreparation2/01-CSV-Parser-Resources/TestApp.Tests/CsvParserTests.cs
+++ Programming Advanced for QA/ExamPreparation2/01-CSV-Parser-Resources/TestApp.Tests/CsvParserTests.cs
@@ -8,24 +8,54 @@ public class CsvParserTests
     [Test]
     public void Test_ParseCsv_EmptyInput_ReturnsEmptyArray()
     {
-        // TODO: finish the test
+        // Arrange
+        string text = "";
+
+        // Act
+        string[] result = CsvParser.ParseCsv(text);
+        
+        // Assert
+        Assert.That(result, Is.Empty);
+
     }
 
     [Test]
     public void Test_ParseCsv_SingleField_ReturnsArrayWithOneElement()
     {
-        // TODO: finish the test
+        // Arrange
+        string text = "lorem";
+
+        // Act
+        string[] result = CsvParser.ParseCsv(text);
+
+        // Assert
+        Assert.That(result.Length, Is.EqualTo(1));
     }
 
     [Test]
     public void Test_ParseCsv_MultipleFields_ReturnsArrayWithMultipleElements()
     {
-        // TODO: finish the test
+        // Arrange
+        string text = "lorem ipsum dolor sit amet";
+        string[] expected = { "lorem ipsum dolor sit amet" };
+        // Act
+        string[] result = CsvParser.ParseCsv(text);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_ParseCsv_TrimsWhiteSpace_ReturnsCleanArray()
     {
-        // TODO: finish the test
+        // Arrange
+        string text = "  lorem   ipsum dolor  sit    amet      ";
+        string[] expected = { "lorem   ipsum dolor  sit    amet" };
+        
+        // Act
+        string[] result = CsvParser.ParseCsv(text);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
