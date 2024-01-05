2b405d779cd80319b244d279703053d054dd7fff
mtsonkova <mtsonkova950@gmail.com>
2024-01-05 10:37:55 +0200

    AMinersTask.cs problem solved

---
+++ Programming Advanced for QA/06-Exercise-Dictionaries-Lambda-And-LINQ/AMinersTask/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace AMinersTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, int> inventory = new Dictionary<string, int>();
+            int counter = 0;
+            string resource = String.Empty;
+            int qty = 0;
+            while(input !="stop")
+            {
+                counter++;
+                if(counter %2 != 0)
+                {
+                    resource = input;
+
+                    if(inventory.ContainsKey(input)== false)
+                    {
+                        inventory[input] = 0;
+                    }
+                } else if(counter % 2 == 0)
+                {
+                    inventory[resource] += int.Parse(input);
+                }
+
+                input = Console.ReadLine();
+                
+            }
+
+            foreach (KeyValuePair<string, int> element in inventory)
+            {
+                Console.WriteLine($"{element.Key} -> {element.Value}");
+            }
+        }
+    }
+}
---
+++ Programming Advanced for QA/06-Exercise-Dictionaries-Lambda-And-LINQ/CountCharsInString/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace CountCharsInString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string result = CountChars(input);
+            Console.WriteLine(result);
+        }
+
+        public static string CountChars(string text)
+        {
+            char[] chars = text.ToCharArray();
+            Dictionary<char, int> charsCount = new Dictionary<char, int>();
+            foreach (char c in chars)
+            {
+                if (c != ' ')
+                {
+                    if (charsCount.ContainsKey(c))
+                    {
+                        charsCount[c] += 1;
+                    }
+                    else
+                    {
+                        charsCount[c] = 1;
+                    }
+
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            foreach (KeyValuePair<char, int> entry in charsCount)
+            {
+                string result = $"{entry.Key} -> {entry.Value}";
+                sb.Append(result);
+                sb.Append(Environment.NewLine);
+
+            }
+            return sb.ToString();
+        }
+    }
+}
