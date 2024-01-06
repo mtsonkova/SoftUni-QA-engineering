4d96bd41d97d2abc5884fb9e6c9961f55b9f5929
mtsonkova <mtsonkova950@gmail.com>
2024-01-07 00:06:48 +0200

    StudentsAcademy problem solved

---
+++ Programming Advanced for QA/06-Exercise-Dictionaries-Lambda-And-LINQ/StudentAcademy/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace StudentAcademy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+            string name = "";
+
+            for (int i = 1; i <= n * 2; i++)
+            {
+               
+                if(i % 2 != 0)
+                {
+                    name = Console.ReadLine();
+                    if(students.ContainsKey(name) == false)
+                    {
+                        students[name] = new List<double>();
+                    }
+                
+                } else if(i % 2 == 0)
+                {
+                    double grade = double.Parse(Console.ReadLine());
+                    students[name].Add(grade);
+                } 
+            }
+
+            foreach(KeyValuePair<string, List<double>> student in students)
+            {
+                double averageGrade = student.Value.Average();
+                if(averageGrade >= 4.5)
+                {
+                    Console.WriteLine($"{student.Key} -> {averageGrade:F2}");
+                }
+            }
+        }
+    }
+}
