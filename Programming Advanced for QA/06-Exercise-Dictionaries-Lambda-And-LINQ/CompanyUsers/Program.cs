277eb56578929abf70add64a7bd4255453db3ac1
mtsonkova <mtsonkova950@gmail.com>
2024-01-07 00:35:38 +0200

    CompanyUsers problem solved

---
+++ Programming Advanced for QA/06-Exercise-Dictionaries-Lambda-And-LINQ/CompanyUsers/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace CompanyUsers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> companies = new Dictionary<string, List<string>>();
+            string userInput = Console.ReadLine();
+
+            while(userInput != "End")
+            {
+                string[] companyInfo = userInput.Split(" -> ");
+                string companyName = companyInfo[0];
+                string employerID = companyInfo[1];
+
+                if(companies.ContainsKey(companyName) == false) 
+                {
+                    companies[companyName] = new List<string>() { employerID};
+                }
+                else
+                {
+                    if (companies[companyName].Contains(employerID) == false)
+                    {
+                        companies[companyName].Add(employerID);
+                    }
+                }
+
+                userInput = Console.ReadLine();
+            }
+
+            foreach (KeyValuePair<string, List<string>> company in companies)
+            {
+                Console.WriteLine(company.Key);
+                foreach (string employerIDs in company.Value)
+                {
+                    Console.WriteLine("-- " + employerIDs);
+
+                }
+            }
+        }
+    }
+}
