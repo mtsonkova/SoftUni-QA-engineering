72a7d54cbd187bbfbd35a663e4b860257517f52b
mtsonkova <mtsonkova950@gmail.com>
2024-01-07 23:13:29 +0200

    Unit tests for problems MinerTest.cs and OddOccurrenceTest.cs completed

--- Programming Advanced for QA/06-Resources-Unit-Testing-Exercise-Dictionaries/TestApp.Tests/MinerTests.cs
+++ Programming Advanced for QA/06-Resources-Unit-Testing-Exercise-Dictionaries/TestApp.Tests/MinerTests.cs
@@ -9,7 +9,14 @@ public class MinerTests
     [Test]
     public void Test_Mine_WithEmptyInput_ShouldReturnEmptyString()
     {
-        // TODO: finish test
+        // Arrange
+        string[] input = Array.Empty<string>();
+
+        // Act
+        string result = Miner.Mine(input);
+        // Assert
+        Assert.That(result, Is.Empty);
+
     }
 
     // TODO: finish test
@@ -17,17 +24,25 @@ public class MinerTests
     public void Test_Mine_WithMixedCaseResources_ShouldBeCaseInsensitive()
     {
         // Arrange
+        string[] input = { "GoLD 8", "SILvER 30" };
 
         // Act
-        //string result = Miner.Mine(input);
+        string result = Miner.Mine(input);
 
         // Assert
-        //Assert.That(result, Is.EqualTo($"gold -> 8{Environment.NewLine}silver -> 30"));
+        Assert.That(result, Is.EqualTo($"gold -> 8{Environment.NewLine}silver -> 30"));
     }
 
     [Test]
     public void Test_Mine_WithDifferentResources_ShouldReturnResourceCounts()
     {
-        // TODO: finish test
+        // Arrange
+        string[] input = { "gold 8", "silver 30", "copper 10", "gold 22" };
+
+        // Act
+        string result = Miner.Mine(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo($"gold -> 30{Environment.NewLine}silver -> 30{Environment.NewLine}copper -> 10"));
     }
 }
--- Programming Advanced for QA/06-Resources-Unit-Testing-Exercise-Dictionaries/TestApp.Tests/OddOccurrencesTests.cs
+++ Programming Advanced for QA/06-Resources-Unit-Testing-Exercise-Dictionaries/TestApp.Tests/OddOccurrencesTests.cs
@@ -9,7 +9,14 @@ public class OddOccurrencesTests
     [Test]
     public void Test_FindOdd_WithEmptyArray_ShouldReturnEmptyString()
     {
-        // TODO: finish test
+        // Arrange
+        string[] words = Array.Empty<string>();
+
+        // Act
+        string result = OddOccurrences.FindOdd(words);
+
+        // Assert
+        Assert.That(result, Is.Empty);
     }
 
     // TODO: finish test
@@ -17,28 +24,51 @@ public class OddOccurrencesTests
     public void Test_FindOdd_WithNoOddOccurrences_ShouldReturnEmptyString()
     {
         // Arrange
+        string[] words = { "home", "word", "home", "word" };
 
         // Act
-        //string result = OddOccurrences.FindOdd(input);
+        string result = OddOccurrences.FindOdd(words);
 
         // Assert
+        Assert.That(result, Is.Empty);        
     }
 
     [Test]
     public void Test_FindOdd_WithSingleOddOccurrence_ShouldReturnTheOddWord()
     {
-        // TODO: finish test
+        // Arrange
+        string[] words = { "home", "word", "home", "word", "home" };
+        string expected = "home";
+        // Act
+        string result = OddOccurrences.FindOdd(words);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_FindOdd_WithMultipleOddOccurrences_ShouldReturnAllOddWords()
     {
-        // TODO: finish test
+        // Arrange
+        string[] words = { "home", "word", "home", "song", "home" };
+        string expected = "home word song" ;
+        // Act
+        string result = OddOccurrences.FindOdd(words);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_FindOdd_WithMixedCaseWords_ShouldBeCaseInsensitive()
     {
-        // TODO: finish test
+        // Arrange
+        string[] words = { "home", "word", "hOme", "song", "HoMe" };
+        string expected = "home word song";
+        // Act
+        string result = OddOccurrences.FindOdd(words);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
