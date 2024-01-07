f636fad1ab1913846820b24b00bb5265f2530d0b
mtsonkova <mtsonkova950@gmail.com>
2024-01-07 23:40:36 +0200

    PlantTest.cs problem solved

--- Programming Advanced for QA/06-Resources-Unit-Testing-Exercise-Dictionaries/TestApp.Tests/PlantsTests.cs
+++ Programming Advanced for QA/06-Resources-Unit-Testing-Exercise-Dictionaries/TestApp.Tests/PlantsTests.cs
@@ -9,7 +9,14 @@ public class PlantsTests
     [Test]
     public void Test_GetFastestGrowing_WithEmptyArray_ShouldReturnEmptyString()
     {
-        // TODO: finish test
+        // Arrange
+        string[] plants = Array.Empty<string>(); 
+        
+        // Act
+        string result = Plants.GetFastestGrowing(plants);
+        
+        // Assert
+        Assert.That(result, Is.Empty);
     }
 
     // TODO: finish test
@@ -17,22 +24,41 @@ public class PlantsTests
     public void Test_GetFastestGrowing_WithSinglePlant_ShouldReturnPlant()
     {
         // Arrange
+        string[] plants = { "rose" };
+        string expected = "Plants with 4 letters:\r\nrose";
 
         // Act
-        //string result = Plants.GetFastestGrowing(plants);
+        string result = Plants.GetFastestGrowing(plants);
 
         // Assert
+        Assert.That(result,Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_GetFastestGrowing_WithMultiplePlants_ShouldReturnGroupedPlants()
     {
-        // TODO: finish test
+        // Arrange
+        string[] plants = { "rose", "lettice", "grapes" };
+        string expected = "Plants with 4 letters:\r\nrose\r\nPlants with 6 letters:\r\ngrapes\r\nPlants with 7 letters:\r\nlettice";
+
+        // Act
+        string result = Plants.GetFastestGrowing(plants);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_GetFastestGrowing_WithMixedCasePlants_ShouldBeCaseInsensitive()
     {
-        // TODO: finish test
+        // Arrange
+        string[] plants = { "rosE", "LeTtIcE", "GrapeS" };
+        string expected = "Plants with 4 letters:\r\nrosE\r\nPlants with 6 letters:\r\nGrapeS\r\nPlants with 7 letters:\r\nLeTtIcE";
+
+        // Act
+        string result = Plants.GetFastestGrowing(plants);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
