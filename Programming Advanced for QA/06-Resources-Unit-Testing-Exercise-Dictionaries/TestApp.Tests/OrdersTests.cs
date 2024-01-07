afc43ed3495dd204de270be073ec2c33a0af7fe3
mtsonkova <mtsonkova950@gmail.com>
2024-01-07 23:30:25 +0200

    OrdersTest.cs task finished

--- Programming Advanced for QA/06-Resources-Unit-Testing-Exercise-Dictionaries/TestApp.Tests/OrdersTests.cs
+++ Programming Advanced for QA/06-Resources-Unit-Testing-Exercise-Dictionaries/TestApp.Tests/OrdersTests.cs
@@ -8,7 +8,14 @@ public class OrdersTests
     [Test]
     public void Test_Order_WithEmptyInput_ShouldReturnEmptyString()
     {
-        // TODO: finish test
+        // Arrange
+        string[] input = Array.Empty<string>();
+
+        // Act 
+        string result = Orders.Order(input);
+
+        // Assert
+        Assert.That(result, Is.Empty);
     }
 
     // TODO: finish test
@@ -16,23 +23,38 @@ public class OrdersTests
     public void Test_Order_WithMultipleOrders_ShouldReturnTotalPrice()
     {
         // Arrange
+        string[] input = { "apple 5.97 1", "banana 3.75 1", "orange 1.98 1" };
 
         // Act
-        //string result = Orders.Order(input);
+       string result = Orders.Order(input);
 
         // Assert
-        //Assert.That(result, Is.EqualTo($"apple -> 5.97{Environment.NewLine}banana -> 3.75{Environment.NewLine}orange -> 1.98"));
+       Assert.That(result, Is.EqualTo($"apple -> 5.97{Environment.NewLine}banana -> 3.75{Environment.NewLine}orange -> 1.98"));
     }
 
     [Test]
     public void Test_Order_WithRoundedPrices_ShouldReturnTotalPrice()
     {
-        // TODO: finish test
+        // Arrange
+        string[] input = { "apple 5.971 2", "banana 3.753 3", "orange 1.982 2" };
+
+        // Act
+        string result = Orders.Order(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo($"apple -> 11.94{Environment.NewLine}banana -> 11.26{Environment.NewLine}orange -> 3.96"));
     }
 
     [Test]
     public void Test_Order_WithDecimalQuantities_ShouldReturnTotalPrice()
     {
-        // TODO: finish test
+        // Arrange
+        string[] input = { "apple 5.971 2.5", "banana 3.753 3.8", "orange 1.982 2.4" };
+
+        // Act
+        string result = Orders.Order(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo($"apple -> 14.93{Environment.NewLine}banana -> 14.26{Environment.NewLine}orange -> 4.76"));
     }
 }
