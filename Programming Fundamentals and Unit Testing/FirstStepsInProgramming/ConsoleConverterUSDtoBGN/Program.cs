959297d443c066d313348600a6eb0b88f6c1e5f1
mtsonkova <mtsonkova950@gmail.com>
2024-01-11 11:48:22 +0200

    Work in progress for FirstStepsInProgramming problems

---
+++ Programming Fundamentals and Unit Testing/FirstStepsInProgramming/ConsoleConverterUSDtoBGN/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Transactions;
+
+namespace ConsoleConverterUSDtoBGN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd = double.Parse(Console.ReadLine());
+            double conversionRate = 1.79549;
+            double leva = usd * conversionRate;
+            Console.WriteLine(leva);
+        }
+    }
+}
---
+++ Programming Fundamentals and Unit Testing/FirstStepsInProgramming/ConverterRADtoDEG/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConverterRADtoDEG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double rad = double.Parse(Console.ReadLine());
+            double degree = (rad * 180) / Math.PI;
+            Console.WriteLine(degree);
+        }
+    }
+}
