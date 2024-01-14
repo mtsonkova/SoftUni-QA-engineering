d74c50dd461b88e28b59adf4dc027d2b90a697ae
mtsonkova <mtsonkova950@gmail.com>
2024-01-15 00:40:34 +0200

    In progress

---
+++ Programming Fundamentals and Unit Testing/ArraysAndLists/BombNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace BombNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(" ").Select(n => int.Parse(n)).ToList();
+            int[] bombInfo = Console.ReadLine().Split(" ").Select(n => int.Parse(n)).ToArray();
+            int bomb = bombInfo[0];
+            int bombPower = bombInfo[1];
+            //1 4 4 2 8 9 1
+            //9 3
+
+            while (nums.Contains(bomb)) {
+                int bombIndex = nums.IndexOf(bomb);
+                int startIndex = bombIndex - bombPower;
+                int endIndex = (bombPower * 2) + 1;
+
+                if(startIndex < 0) { startIndex = 0; }
+                if(endIndex >  nums.Count - 1) { endIndex = nums.Count - 1; }
+
+                nums.RemoveRange(startIndex, endIndex);
+                          }
+
+            int sum = nums.Sum();
+
+            Console.WriteLine(sum);
+
+            //1 4 4 2 8 9 1
+            //9 3
+
+        }
+    }
+}
---
+++ Programming Fundamentals and Unit Testing/ArraysAndLists/CardsGame/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace CardsGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstPlayerDeck = Console.ReadLine().Split(" ").Select(n => int.Parse(n)).ToList();
+            List<int> secondPlayerDeck = Console.ReadLine().Split(" ").Select(n => int.Parse(n)).ToList();
+
+            while(firstPlayerDeck.Count > 0 && secondPlayerDeck.Count > 0)
+            {
+                int firstPlayerCard = firstPlayerDeck[0];
+                int secondPlayerCard = secondPlayerDeck[0];
+
+                if (firstPlayerCard == secondPlayerCard)
+                {
+                    firstPlayerDeck.Remove(firstPlayerCard);
+                    secondPlayerDeck.Remove(secondPlayerCard);
+                }
+
+                else if(firstPlayerCard > secondPlayerCard)
+                {
+                   firstPlayerDeck.Add(firstPlayerCard);
+                   firstPlayerDeck.RemoveAt(0);
+                    firstPlayerDeck.Add(secondPlayerCard);
+                    secondPlayerDeck.RemoveAt(0);
+                }
+                else
+                {
+                    secondPlayerDeck.Add(secondPlayerCard);
+                    secondPlayerDeck.RemoveAt(0);
+                    secondPlayerDeck.Add(firstPlayerCard);
+                    firstPlayerDeck.RemoveAt(0);
+                }
+            }
+
+            if(firstPlayerDeck.Count > 0)
+            {
+                int sum = firstPlayerDeck.Sum();
+                Console.WriteLine($"First player wins! Sum: {sum}");
+            } 
+            else
+            {
+                int sum = secondPlayerDeck.Sum();
+                Console.WriteLine($"Second player wins! Sum: {sum}");
+            } 
+        }
+    }
+}
