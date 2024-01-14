f7739467056096ca3e29dced9281256ea055d090
mtsonkova <mtsonkova950@gmail.com>
2024-01-14 22:58:47 +0200

    Work in progress

---
+++ Programming Fundamentals and Unit Testing/ArraysAndLists/ArrayRotation/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ArrayRotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] arr = Console.ReadLine().Split(" ").Select(n => int.Parse(n)).ToArray();
+           int num = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < num; i++)
+            {
+                int firstNum = arr[0];
+                for(int j = 0; j < arr.Length - 1; j++)
+                {
+                    arr[j] = arr[ j+ 1];
+                }
+                arr[arr.Length - 1] = firstNum;
+            }
+
+            Console.WriteLine(string.Join(" ", arr));
+
+
+        }
+    }
+}
