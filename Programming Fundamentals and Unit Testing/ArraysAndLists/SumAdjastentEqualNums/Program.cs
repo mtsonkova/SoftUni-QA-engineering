54449dc928b41ec9399d910230071f4491a1965b
mtsonkova <mtsonkova950@gmail.com>
2024-01-15 23:22:47 +0200

    SumAdjustentEqualNums solved

---
+++ Programming Fundamentals and Unit Testing/ArraysAndLists/SumAdjastentEqualNums/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace SumAdjastentEqualNums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            bool hasAdjustentEqualNums = true;
+
+            while (HasAdjacentEqualNums(nums))
+            {
+                for (int i = 0; i < nums.Count - 1; i++)
+                {
+                    int firstNum = nums[i];
+                    int secondNum = nums[i + 1];
+                    if (firstNum == secondNum)
+                    {
+                        firstNum += secondNum;
+                        nums[i] = firstNum;
+                        nums.Remove(secondNum);
+                    }
+
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", nums));
+        }
+
+        static bool HasAdjacentEqualNums(List<int> nums)
+        {
+            for (int i = 0; i < nums.Count - 1; i++)
+            {
+                // Check if the current element is equal to the next element
+                if (nums[i] == nums[i + 1])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
