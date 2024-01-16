33a69190897145d36861c46d83922480513c2e3d
mtsonkova <mtsonkova950@gmail.com>
2024-01-16 07:49:22 +0200

    Merging arrays problem solved

---
+++ Programming Fundamentals and Unit Testing/ArraysAndLists/CondenseCollectionToNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace CondenseCollectionToNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            while(nums.Count > 2)
+            {
+                for(int i = 0; i < nums.Count - 2; i++)
+                {
+                    
+                        nums[i] += nums[i + 1];
+                        nums[i + 2] += nums[i + 1];
+                        nums.Remove(nums[i + 1]);
+                    
+                }
+            }
+
+            Console.WriteLine(nums.Count);
+            Console.WriteLine(nums[1]);
+
+        }
+    }
+}
---
+++ Programming Fundamentals and Unit Testing/ArraysAndLists/MergingLists/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http.Headers;
+
+namespace MergingLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> first = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            List<int> second = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            List<int> merged = new List<int>();
+
+            bool isFirstBigger = first.Count > second.Count;
+
+            int n = Math.Min(first.Count, second.Count);
+
+            for(int i = 0; i < n; i++)
+            {
+                merged.Add(first[i]);
+                merged.Add(second[i]);
+            }
+
+            if(isFirstBigger)
+            {
+                for(int i = n; i < first.Count; i++)
+                {
+                    merged.Add(first[i]);
+                }
+            } else
+            {
+                for (int i = n; i < second.Count; i++)
+                {
+                    merged.Add(second[i]);
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", merged));
+        }
+    }
+}
