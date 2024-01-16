37d570aa4ced56988f5c4fbfa28dd3be84888cbb
mtsonkova <mtsonkova950@gmail.com>
2024-01-16 15:54:09 +0200

    All problems from ArraysAndLists are solved

---
+++ Programming Fundamentals and Unit Testing/ArraysAndLists/CondenseArrToNum/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CondenseArrToNum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            while (nums.Count > 1)
+            {
+                for (int i = 0; i < nums.Count - 1; i++)
+                {
+                        nums[i] += nums[i + 1];                                   
+
+                }
+                nums.Remove(nums[nums.Count - 1]);
+               
+            }
+            Console.WriteLine(nums[0]);
+        }
+    }
+}
---
+++ Programming Fundamentals and Unit Testing/ArraysAndLists/GausTrick/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace GausTrick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            List<int> nums = new List<int>();
+
+            for(int i = 0; i < numbers.Length / 2; i++) {
+                int firstNum = numbers[i];
+                int secondNum = numbers[numbers.Length - 1 - i];
+                nums.Add(firstNum + secondNum);
+            }
+
+            if(numbers.Length % 2 != 0)
+            {
+                nums.Add(numbers[numbers.Length / 2]);
+            }
+
+            Console.WriteLine(String.Join(" ", nums));
+        }
+    }
+}
