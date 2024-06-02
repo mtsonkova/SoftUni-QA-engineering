f7c68ce94f202587caa91ad244f4b82bbcc492f8
MTsonkova <mtsonkov950@gmail.com>
2024-06-02 18:04:32 +0300

    In progress

--- Frontend Technologies Basics/05-JavaScript-Async-Functions-Exercise-I-Resources/05-Promise-with-Multiple-Hadlers/app.js
+++ Frontend Technologies Basics/05-JavaScript-Async-Functions-Exercise-I-Resources/05-Promise-with-Multiple-Hadlers/app.js
@@ -1,3 +1,16 @@
 function promiseWithMultipleHandlers() {
-    // TODO
+    let promise = new Promise(function(resolve, reject) {
+        setTimeout(function(){
+            resolve('Hello World');
+        }, 2000);
+    });
+
+    promise.then(function(result) {
+        console.log(result);   
+        return result;     
+    })
+    .then(function(result){
+        console.log(result);
+    });
+   
 }
