92ed72771086f69ca24c4b4d74495201403272e2
MTsonkova <mtsonkov950@gmail.com>
2024-06-02 20:37:14 +0300

    In progress

--- Frontend Technologies Basics/05-JavaScript-Async-Functions-Exercise-I-Resources/10-Chained-Promises-with-Async-Await/app.js
+++ Frontend Technologies Basics/05-JavaScript-Async-Functions-Exercise-I-Resources/10-Chained-Promises-with-Async-Await/app.js
@@ -1,3 +1,20 @@
 async function chainedPromisesAsync() {
-    // TODO
+   let promise = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        resolve('1');
+        setTimeout(function() {
+            resolve('2');
+            setTimeout(function(){
+                resolve('3');
+            }, 1000)
+        }, 1000)
+    }, 1000)
+   })
+
+    await promise.then(function(result) {
+        console.log(result);
+    });    
 }
+
+let button = document.querySelector("button");
+button.addEventListener('click', chainedPromisesAsync());
