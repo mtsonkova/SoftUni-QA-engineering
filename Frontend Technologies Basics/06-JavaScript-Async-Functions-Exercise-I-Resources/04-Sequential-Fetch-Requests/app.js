b7d1fb4409c212799320d05862401f78185c8e76
MTsonkova <mtsonkov950@gmail.com>
2024-06-04 14:39:43 +0300

    In progress

--- Frontend Technologies Basics/06-JavaScript-Async-Functions-Exercise-I-Resources/04-Sequential-Fetch-Requests/app.js
+++ Frontend Technologies Basics/06-JavaScript-Async-Functions-Exercise-I-Resources/04-Sequential-Fetch-Requests/app.js
@@ -1,3 +1,6 @@
 async function fetchSequential() {
-      // TODO...
+      const firstResponse = await fetch('https://swapi.dev/api/people/1').then(res => res.json());
+      console.log(firstResponse);
+      const secondResponse = await fetch('https://swapi.dev/api/people/2').then(res => res.json());
+      console.log(secondResponse);
 }
